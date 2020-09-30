71b20c436a6a5f50fa429cd773a63dd05a959fba
Alvaro Montes <alvaro.montes@zagalabs.com>
2020-09-29 19:17:46 -0500

    initial changes

---
+++ precache-manifest.ed6d16333c2ed1d36f63e67342660ee2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1469fbe6273f9b6af151fcfb8f638525",
+    "url": "/scheduler/index.html"
+  },
+  {
+    "revision": "45a0c9d39856ad240950",
+    "url": "/scheduler/static/css/main.c3e297e3.chunk.css"
+  },
+  {
+    "revision": "e4aa676aafe3ca8a39a7",
+    "url": "/scheduler/static/js/2.90dd8756.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/scheduler/static/js/2.90dd8756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a0c9d39856ad240950",
+    "url": "/scheduler/static/js/main.11c4d978.chunk.js"
+  },
+  {
+    "revision": "3860ac4b1f75b8771f1e",
+    "url": "/scheduler/static/js/runtime-main.1f13b186.js"
+  },
+  {
+    "revision": "4c91fd8a53d2f07a6120110c4f81626f",
+    "url": "/scheduler/static/media/0.4c91fd8a.png"
+  },
+  {
+    "revision": "583d1fc2f0176b54ac6b6c61163c46ac",
+    "url": "/scheduler/static/media/bitmap@2x.583d1fc2.png"
+  },
+  {
+    "revision": "0a17f8efb7a54bbb620144823b46a0d5",
+    "url": "/scheduler/static/media/eye.0a17f8ef.png"
+  },
+  {
+    "revision": "521c8f1281bbefb4b9a0f981a5db9099",
+    "url": "/scheduler/static/media/kitchen.521c8f12.png"
+  },
+  {
+    "revision": "5cb4b0d81bd1ac2a4ebb10325a062111",
+    "url": "/scheduler/static/media/laptop-j-j-3.5cb4b0d8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scheduler/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f4aa53d8a021fe0163d945685d9609c3",
+    "url": "/scheduler/static/media/maria.f4aa53d8.png"
+  },
+  {
+    "revision": "be6a6ea9cbf6ddc8bfda9da098e358c3",
+    "url": "/scheduler/static/media/zagalogo-dark-8.be6a6ea9.svg"
+  }
+]);
